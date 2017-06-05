0e0474d67bbed6bba02fec0cddd28232fa5c5a07
Richard Kavanagh <scsrek@leeds.ac.uk>
2017-06-05 12:18:34 +0100

    Integrating the environment monitor into the data source.

--- src/main/java/eu/tango/self/adaptation/manager/listeners/EnvironmentMonitor.java
+++ src/main/java/eu/tango/self/adaptation/manager/listeners/EnvironmentMonitor.java
@@ -15,11 +15,10 @@
  */
 package eu.tango.self.adaptation.manager.listeners;
 
+import eu.tango.energymodeller.datasourceclient.CollectDNotificationHandler;
 import eu.tango.energymodeller.datasourceclient.CollectdDataSourceAdaptor;
 import eu.tango.energymodeller.datasourceclient.HostDataSource;
 import eu.tango.energymodeller.datasourceclient.HostMeasurement;
-import eu.tango.energymodeller.datasourceclient.Measurement;
-import eu.tango.self.adaptation.manager.model.SLALimits;
 import eu.tango.self.adaptation.manager.rules.EventAssessor;
 import eu.tango.self.adaptation.manager.rules.datatypes.EventData;
 import java.util.HashMap;
@@ -38,12 +37,12 @@ import org.jcollectd.agent.protocol.Dispatcher;
  *
  * @author Richard Kavanagh
  */
-public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable {
+public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable, CollectDNotificationHandler {
 
     private EventAssessor eventAssessor;
     private HostDataSource datasource = new CollectdDataSourceAdaptor();
     private boolean running = true;
-    
+
     private static final Map<String, EventData.Operator> OPERATOR_MAPPING
             = new HashMap<>();
 
@@ -60,7 +59,13 @@ public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable {
         OPERATOR_MAPPING.put("GREATER_EQUAL", EventData.Operator.GTE);
         OPERATOR_MAPPING.put(null, null);
         OPERATOR_MAPPING.put("", null);
-    }    
+    }
+
+    public EnvironmentMonitor() {
+        if (datasource instanceof CollectdDataSourceAdaptor) {
+            ((CollectdDataSourceAdaptor) datasource).setNotificationHandler(this);
+        }
+    }
 
     @Override
     public void setEventAssessor(EventAssessor assessor) {
@@ -82,6 +87,10 @@ public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable {
         try {
             // Wait for a message
             while (running) {
+                /**
+                 * TODO Fix the measurement contains no concept of breach.
+                 * Thus QoS criteria need implementing
+                 */
                 //TODO Implement here
                 //GET LIST OF Terms to monitor here
                 //Compare them to the data source
@@ -98,79 +107,31 @@ public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable {
             ex.printStackTrace();
         }
     }
-    
-    /**
-     * @param measurement The incoming event to convert into the self-adaptation
-     * managers internal representation
-     * @return The converted internal representation of events
-     * @see eu.ascetic.paas.slam.pac.events.ViolationMessage
-     */
-    public static EventData convertEventData(Measurement measurement, SLALimits criteria) {
-        EventData answer = new EventData();
-        /**
-         * TODO Fix the measurement contains no concept of breach, this already needs to exist
-         * for a conversion method to work.
-         */
-        /**
-         * Any new event may be either system orientated or application oriented
-         * this will depend on the source of the event
-         */
-//        answer.setApplicationId(measurement.getAppId());
-//        answer.setDeploymentId(measurement.getDeploymentId());
-        
-        answer.setTime(measurement.getClock());
-        long now = System.currentTimeMillis();
-        now = now / 1000;
-        if (answer.getTime() > now) {
-            answer.setTime(now);
-        }
-        //TODO May not be the 0th SLA Term FIX HERE
-        if (criteria.getQosCriteria().get(0).getSeverity().equals("violation")) {
-            answer.setType(EventData.Type.SLA_BREACH);
-        } else if (criteria.getQosCriteria().get(0).getSeverity().equals("warning")) {
-            answer.setType(EventData.Type.WARNING);
-        } else {
-            answer.setType(EventData.Type.OTHER);
-        }
-//        answer.setRawValue(Double.parseDouble(measurement.getValue().getTextValue()));
-//        answer.setGuranteedValue(measurement.getAlert().getSlaGuaranteedState().getGuaranteedValue());
-//        answer.setGuranteeOperator(getOperator(measurement));
-//
-//        answer.setSlaUuid(measurement.getAlert().getSlaUUID());
-//        answer.setGuaranteeid(measurement.getAlert().getSlaGuaranteedState().getGuaranteedId());
-//        answer.setAgreementTerm(measurement.getAlert().getSlaAgreementTerm());
-        return answer;
 
-    }
-    
     /**
-     * @param notification The incoming event to convert into the self-adaptation
-     * managers internal representation
+     * @param notification The incoming event to convert into the
+     * self-adaptation managers internal representation
      * @return The converted internal representation of events
      * @see eu.ascetic.paas.slam.pac.events.ViolationMessage
      */
-    public static EventData convertEventData(Notification notification, SLALimits criteria) {
+    public static EventData convertEventData(Notification notification) {
         EventData answer = new EventData();
         /**
-         * TODO Fix the measurement contains no concept of breach, this already needs to exist
-         * for a conversion method to work.
-         */
-        /**
-         * Any new event may be either system orientated or application oriented
-         * this will depend on the source of the event
+         * TODO Fix commented out code sections Any new event may be either
+         * system orientated or application oriented this will depend on the
+         * source of the event
          */
 //        answer.setApplicationId(measurement.getAppId());
 //        answer.setDeploymentId(measurement.getDeploymentId());
-        
-        answer.setTime(notification.getTime());
+
+        answer.setTime(notification.getTime() >> 30);
         long now = System.currentTimeMillis();
-        now = now / 1000;
         if (answer.getTime() > now) {
             answer.setTime(now);
         }
         //TODO May not be the 0th SLA Term FIX HERE
         /**
-         *Options available@ FAILURE(1), WARNING(2), UNKNOWN(3), OKAY(4);
+         * Options available@ FAILURE(1), WARNING(2), UNKNOWN(3), OKAY(4);
          */
         if (notification.getSeverity().equals(Severity.FATAL)) {
             answer.setType(EventData.Type.SLA_BREACH);
@@ -188,8 +149,8 @@ public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable {
         answer.setAgreementTerm(notification.getMessage());
         return answer;
 
-    }    
-    
+    }
+
     /**
      * This converts a violation messages guarantee state's operator into an
      * event type operator.
@@ -201,19 +162,24 @@ public class EnvironmentMonitor implements EventListener, Dispatcher, Runnable {
         //TODO find the terms that setup the notification event
 //        return OPERATOR_MAPPING.get(notification.getOperator());
         return EventData.Operator.LT;
-    }    
+    }
 
     @Override
     public void dispatch(Values values) {
-        throw new UnsupportedOperationException("Not supported yet.");
         /**
-         * The main element of this is to listen for notifications of CollectD threshold limits.
+         * The main element of this is to listen for notifications of CollectD
+         * threshold limits. Therefore this is not listened to and values
+         * arriving are ignored.
+         * 
+         * Consider route i.e. through adaptor or through this mechanism here. 
          */
     }
 
     @Override
     public void dispatch(Notification ntfctn) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EventData data = convertEventData(ntfctn);
+        eventAssessor.assessEvent(data);
+
     }
 
 }
