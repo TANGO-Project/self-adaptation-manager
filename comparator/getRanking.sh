6ab685c77257d485238c724a8926032f5727b47d
Richard Kavanagh <scsrek@leeds.ac.uk>
2017-10-12 12:15:25 +0100

    Adjusting output text of the script.

--- comparator/getRanking.sh
+++ comparator/getRanking.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 1 ]; then
   echo "The idea behind the usage of this script is that different implementations of the same application are to be comapared with the same workload."
   echo "This therefore from existing ranking data for a named application indicates which accelerator implementation is fastest."
-  echo "Script usage: post_run_processing.sh <script_name>"
+  echo "Script usage: getRanking.sh <script_name>"
   exit;
 fi
 
